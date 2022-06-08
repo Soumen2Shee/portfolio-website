f281444012479af3da37f52b311ab874499d3a3b
Soumen Shee <soumen12shee@gmail.com>
2022-06-08 21:13:45 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// ======= typing animation =====
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Devloper","Computer Science Engineering Student"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+}
+);
